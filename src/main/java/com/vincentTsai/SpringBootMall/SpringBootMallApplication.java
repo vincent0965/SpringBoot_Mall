353b33d1af46ce9fbe7fd01fee22de75deb4182e
DESKTOP-PEPGN09\vincent <vincent0965@gmail.com>
2023-08-03 19:00:53 +0800

    初始化SpringBoot

---
+++ src/main/java/com/vincentTsai/SpringBootMall/SpringBootMallApplication.java
@@ -0,0 +1,13 @@
+package com.vincentTsai.SpringBootMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincentTsai/SpringBootMall/SpringBootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincentTsai.SpringBootMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
